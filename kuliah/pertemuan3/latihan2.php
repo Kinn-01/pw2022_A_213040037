d302f980e7257fbd1ea99cf11bc86013c9cb5d08
gilmanarief <gilmanarief23@gmail.com>
2022-02-26 10:09:55 +0700

    Latihan 2 pertemuan 3

--- kuliah/pertemuan3/latihan2.php
+++ kuliah/pertemuan3/latihan2.php
@@ -1,5 +1,30 @@
 <?php 
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
 
+for( $i = 0; $i < 5; $i++ ) {
+    echo "Hello World! <br>";
+}
+echo "<br>";
+echo "<hr>";
+
+$i = 0;
+while( $i < 5 ) {
+    echo "Hello World! <br>";
+$i++;
+}
+
+echo "<br>";
+echo "<hr>";
+
+$i = 10;
+do {
+    echo "Hello World! <br>";
+$i++;
+} while( $i < 5 );
 
 
 ?>
